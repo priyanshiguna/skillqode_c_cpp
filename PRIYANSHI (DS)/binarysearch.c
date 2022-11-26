216a630565e13feb50df2935148314442d468af0
priyanshiguna <gunapriyanshi37@gmail.com>
2022-11-26 12:03:14 +0530

    first commit

--- PRIYANSHI (DS)/binarysearch.c
+++ PRIYANSHI (DS)/binarysearch.c
@@ -2,29 +2,6 @@
 #include<stdlib.h>
 int i, j, n, temp;
 int a[100];
-void randomarray()
-{
-    int a[10];
-    for(int i=0;i<10;i++)
-        a[i]=rand()%100;
-    for(int i=0;i<10;i++)
-        printf("\n a[ %d ] : %d" , i , a[i]);    
-}
-void linearsearch()
-{
-    int n ,i, x;
-    printf("\n enter a number to search" );
-    scanf("%d",&x);
-
-    for(int i=0;i<n;i++)
-    {
-      if (a[i] == x)   
-      {
-        printf("\n %d : at position %d .", x, i+1);
-      }
-    }
-    return i+1;
-} 
 void sorting()
 {
     printf("Enter Size Of Array : ");
@@ -75,37 +52,7 @@ void  binarysearch()
     if(low > high)
     printf("\n not found ! %d ", x);
 }
-int main()
+int main ()
 {
-  {
-    int ch,val;
-    do 
-    {
-        printf("\n press 1 for-->> print random array");
-        printf("\n press 2 for-->> selection sorting");
-        printf("\n press 3 for-->> linear search");
-        printf("\n press 4 for-->> binary search");
-        printf("\n press 0 for-->> exit");
-        printf("\nenter your choice=");
-        scanf("%d",&ch);
-        switch (ch)
-        {
-        case 1:
-            randomarray();
-            break;
-        case 2:
-            sorting();
-            break;
-        case 3:
-            linearsearch();
-            break;
-        case 4:
-            binarysearch();
-            break;
-        case 0:
-            break;
-        }
-    }
-    while(ch!=0);
-}
+    binarysearch();
 }
