0d6fc57d34785c1b9a657341bb6f7be0ca1f8dc0
priyanshiguna <gunapriyanshi37@gmail.com>
2022-12-01 11:32:22 +0530

    first commit

---
+++ PRIYANSHI (DS)/marge.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#define SIZE 10
+void mergearray()
+{
+    int a[10], b[10], i, k, merge[50];
+    printf("\n Enter Array 1 Elements: ");
+    for(i=0; i<SIZE; i++)
+    {
+        scanf("%d", &a[i]);
+        merge[i] = a[i];
+    }
+    k = i;
+    printf("\n Enter Array 2 Elements: ");
+    for(i=0; i<SIZE; i++)
+    {
+        scanf("%d", &b[i]);
+        merge[k] = b[i];
+        k++;
+    }
+    printf("\nThe new array after merging is:\n");
+    for(i=0; i<k; i++)
+        printf("%d ", merge[i]);
+}
+int main()
+{
+    mergearray();
+     return 0;
+}
